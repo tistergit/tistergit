e873c01f4285588fb3b92cd701c1c2edcda15776
tister <tister@tister-PC>
2011-08-14 07:34:48 +0800

    first commit

---
+++ learn/views.py
@@ -0,0 +1,12 @@
+'''
+Created on 2011-8-13
+
+@author: tister
+'''
+from django.shortcuts import render_to_response
+from django.contrib.auth.decorators import login_required
+
+
+@login_required
+def hello(request):
+    return render_to_response('a.html')
